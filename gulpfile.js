a9dca61237a28bf31cc6d0489aed90c4b445514b
Jose Granado <jose.granado2014@gmail.com>
2018-02-17 04:57:33 -0400

    Agregando favicon y organizando assets

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,22 @@ const gulp = require('gulp');
 
 const sass = require('gulp-sass');
 
+const rename = require('gulp-rename');
+
+
 gulp.task('styles', () => 
 {
     gulp
-    .src('public/app.sass')
+    .src('app.sass')
     .pipe(sass())
-    .pipe(gulp.dest('public'));
+    .pipe(gulp.dest('public/css'));
 
 })
 
-gulp.task('default',['styles']);
+gulp.task('assets', () => 
+{
+    gulp
+    .src('assets/*/*')
+    .pipe(gulp.dest('public'))
+})
+gulp.task('default',['styles','assets']);
